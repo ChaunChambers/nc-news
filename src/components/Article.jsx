7ab77d29eaabaf358498ed0f35ef0498c60442a0
ChaunChambers <chaunchambers@gmail.com>
2024-09-18 18:11:05 +0100

    Added loading states - Lottie not active

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -10,10 +10,12 @@ const Article = () => {
   const [article, setArticle] = useState({ title: "Not Found" });
   const [count, setCount] = useState(article.votes);
   const [counter, setCounter] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     getArticle(article_id).then((article) => {
       setArticle(article);
       setCount(article.votes);
+      setIsLoading(false);
     });
   }, []);
 
@@ -38,50 +40,56 @@ const Article = () => {
   function handleNavigate() {
     navigate("./#form");
   }
+  if (isLoading) {
+    return (
+      <>
+        <h2>Loading...</h2>
+      </>
+    );
+  } else
+    return (
+      <div>
+        {article.title != "Not Found" && article.title ? (
+          <div className="one-article-div" id="top">
+            <h3>Title: {article.title}</h3>
+            <h4>Topic: {article.topic}</h4>
+            <h4>Author: {article.author}</h4>
+            <p>{article.body}</p>
+            <h5>Created: {article.created_at}</h5>
+            <h5>Votes: {count}</h5>
+            <input
+              type="submit"
+              onClick={(e) => handleVote(e)}
+              value="Vote Up"
+              className="input-submit margin-left-right"
+            />
 
-  return (
-    <div>
-      {article.title != "Not Found" && article.title ? (
-        <div className="one-article-div" id="top">
-          <h3>Title: {article.title}</h3>
-          <h4>Topic: {article.topic}</h4>
-          <h4>Author: {article.author}</h4>
-          <p>{article.body}</p>
-          <h5>Created: {article.created_at}</h5>
-          <h5>Votes: {count}</h5>
-          <input
-            type="submit"
-            onClick={(e) => handleVote(e)}
-            value="Vote Up"
-            className="input-submit margin-left-right"
-          />
+            <input
+              type="submit"
+              onClick={(e) => handleVote(e)}
+              value="Vote Down"
+              className="input-submit margin-left-right"
+            />
 
-          <input
-            type="submit"
-            onClick={(e) => handleVote(e)}
-            value="Vote Down"
-            className="input-submit margin-left-right"
-          />
-
-          <HashLink to="#form" className="input-submit margin-left-right">
-            Post Comment
-          </HashLink>
-          <p>Comment Count: {article.comment_count}</p>
-        </div>
-      ) : (
-        <>
-          <h2>Not Found</h2>
-          <br />
-          <Link to={"/"}>Home</Link>
-          {/* {useEffect(() => {
+            <HashLink to="#form" className="input-submit margin-left-right">
+              Post Comment
+            </HashLink>
+            <p>Comment Count: {article.comment_count}</p>
+          </div>
+        ) : (
+          <>
+            <h2>Not Found</h2>
+            <br />
+            <Link to={"/"}>Home</Link>
+            {/* {useEffect(() => {
             navigate("/");
           }, [])} */}
-        </>
-      )}
+          </>
+        )}
 
-      <Comments article_id={article.article_id} />
-    </div>
-  );
+        <Comments article_id={article.article_id} />
+      </div>
+    );
 };
 
 export default Article;
