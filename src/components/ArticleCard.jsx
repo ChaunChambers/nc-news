d99e026557031ff8d2893e525ab8995a390849d0
ChaunChambers <chaunchambers@gmail.com>
2024-09-19 12:20:52 +0100

    this commit will match date with regex so it doesn't show the time

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 
 const ArticleCard = (article) => {
+  const date = article.article.created_at.match(/^20\d\d\-\d\d\-\d\d/);
+
   return (
     <div className="article-div-main">
       <h3>{article.article.title}</h3>
       <h4>Topic: {article.article.topic}</h4>
       <h4>Author: {article.article.author}</h4>
-      <h5>Created: {article.article.created_at}</h5>
+      <h5>Created: {date[0]}</h5>
       <h5>Votes: {article.article.votes}</h5>
       <p>Comment Count: {article.article.comment_count}</p>
       <Link
