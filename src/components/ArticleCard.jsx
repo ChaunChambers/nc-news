26874431fcf04e720cfb8a1636d8271a2efbb987
ChaunChambers <chaunchambers@gmail.com>
2024-09-25 15:27:18 +0100

    Bootstrap styling added

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,23 +1,31 @@
 import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
 
 const ArticleCard = (article) => {
   const date = article.article.created_at.match(/^20\d\d\-\d\d\-\d\d/);
 
   return (
-    <div className="article-div-main">
-      <h3>{article.article.title}</h3>
-      <h4>Topic: {article.article.topic}</h4>
-      <h4>Author: {article.article.author}</h4>
-      <h5>Created: {date[0]}</h5>
-      <h5>Votes: {article.article.votes}</h5>
-      <p>Comment Count: {article.article.comment_count}</p>
-      <Link
-        to={`../articles/${article.article.article_id}`}
-        className="input-submit "
-      >
-        View Article
-      </Link>
-    </div>
+    <Card style={{ width: "18rem" }}>
+      <Card.Body>
+        <Card.Title>{article.article.title}</Card.Title>
+        <h5>Author: {article.article.author}</h5>
+
+        <Card.Subtitle className="mb-2 text-muted">
+          Topic: {article.article.topic}
+        </Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted">
+          Created: {date[0]}
+        </Card.Subtitle>
+        <h5>Votes: {article.article.votes}</h5>
+        <p>Comment Count: {article.article.comment_count}</p>
+        <Link
+          to={`../articles/${article.article.article_id}`}
+          className="input-submit "
+        >
+          View Article
+        </Link>
+      </Card.Body>
+    </Card>
   );
 };
 
