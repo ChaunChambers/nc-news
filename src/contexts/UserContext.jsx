db5ef7dbd527e731c3fb20554b5edc6dd397adbb
ChaunChambers <chaunchambers@gmail.com>
2024-10-08 18:42:40 +0100

    changed user login state to null

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -3,12 +3,7 @@ import { createContext, useState } from "react";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [userLoggedIn, setUserLoggedIn] = useState({
-    username: "tickle122",
-    name: "Tom Tickle",
-    avatar_url:
-      "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
-  });
+  const [userLoggedIn, setUserLoggedIn] = useState(null);
 
   function handleLogOut() {
     setUserLoggedIn(null);
